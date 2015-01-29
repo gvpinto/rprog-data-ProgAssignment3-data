dc4ef59133ce3f1667e5cc0b563122f02f805285
Glenn Pinto <gvpinto@gmail.com>
2015-01-28 22:48:19 -0500

    Initial Commit

---
+++ best.R
@@ -0,0 +1,15 @@
+best <- function(state, outcome) {
+    
+    errorMsg <- paste(c('Error in best("', state, '", "', outcome, '") : invalid '), collapse="")
+    
+    if ("Heart Attack") {
+        
+    } else if("Heart Failure") {
+        
+    } else if ("pneumonia") {
+        
+    } else {
+        paste(c(errorMsg, 'outcome', collapse="")
+    }
+    
+}
